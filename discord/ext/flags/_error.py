82326435cadd3bacbe7f3dca22b637e3804dffc5
Xua Yraili <williamblakelock.wb@gmail.com>
2019-04-18 21:17:13 +1200

    use UserInputError instead of BaseException

--- discord/ext/flags/_error.py
+++ discord/ext/flags/_error.py
@@ -1,3 +1,6 @@
-class FlagParsingError(BaseException):
+from discord.ext.commands import UserInputError
+
+
+class FlagParsingError(UserInputError):
     def __init__(self, message):
         super().__init__(message)
