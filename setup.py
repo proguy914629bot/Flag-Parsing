2a58e6d7a32f11eff865612cf8eca3f32c3afc06
Xua Yraili <williamblakelock.wb@gmail.com>
2019-04-24 13:53:15 +1200

    MIT > GPL for this kind of thing

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     version=version,
     url="https://github.com/XuaTheGrate/Flag-Parsing",
     packages=['discord.ext.flags'],
-    license='GPLv3',
+    license='MIT',
     description="A Discord.py extension allowing you to pass flags as arguments.",
     long_description=readme,
     long_description_content_type="text/markdown",
@@ -20,7 +20,7 @@ setup(
     extras_require=None,
     classifiers=[
         'Development Status :: 5 - Production/Stable',
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'License :: OSI Approved :: MIT License',
         'Intended Audience :: Developers',
         'Natural Language :: English',
         'Operating System :: OS Independent',
